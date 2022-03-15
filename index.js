322243a1ffff0e03178a43924d8334aa76adae91
isaacpitwa <isaacpitwa256@gmail.com>
2022-03-15 19:21:52 +0300

    Fix localStorage bug

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 let books = [];
 
-class Book {
-  constructor(title, author) {
-    this.title = title;
-    this.author = author;
-    this.id = books.length + 1;
-  }
-}
-
 const addBook = (title, author) => {
-  books.push(new Book(title, author));
+  const awesomeBook = {
+    title: title,
+    author: author,
+    id: books.length + 1
+  }
+  books.push(awesomeBook);
 };
 
 const removeBook = (id) => {
@@ -62,25 +59,25 @@ const form = document.querySelector('form');
 const author = form.querySelector('#author');
 const title = form.querySelector('#title');
 
-const rmvListener = () => {
-  for (let i = 0; i < books.length; i++) {
-    let removeBtn = document.querySelector('.remove-' + i);
-    console.log(removeBtn);
-  }
-};
-
 const saveData = () => {
-  for (const book in books) {
-    localStorage.setItem(book.title, JSON.stringify(book.author));
+  localStorage.setItem("myBooks", JSON.stringify(books));
+};
+
+const getData = () => {
+  const formData = JSON.parse(localStorage.getItem("myBooks"));
+  if (formData == null) {
+    books = [];
+  } else {
+    books = formData;
   }
 };
 
-window.addEventListener('load', () => {
-  const formData = JSON.parse(localStorage.getItem('Data')) || [];
-  books = formData;
-});
+window.onload = getData();
+window.onbeforeunload = function() {
+  getData();
+  displayBooks();
+}
 
-window.onload = displayBooks();
 const addBtn = document.querySelector('#add-btn');
 addBtn.addEventListener('click', (e) => {
   e.preventDefault();
@@ -88,4 +85,8 @@ addBtn.addEventListener('click', (e) => {
   bookAuthor = author.value;
   addBook(bookTitle, bookAuthor);
   displayBooks();
+  saveData();
 });
+
+getData();
+displayBooks();
