78709071a08bb5a32dc3199f7130f4f637f510ff
isaacpitwa <isaacpitwa256@gmail.com>
2022-03-15 19:19:06 +0300

    Fix JS logical errors

--- index.js
+++ index.js
@@ -1,36 +1,60 @@
 let books = [];
+
+class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+    this.id = books.length + 1;
+  }
+}
+
 const addBook = (title, author) => {
-  books.push({ title, author });
+  books.push(new Book(title, author));
 };
-const removeBook = (title) => {
-  books = books.filter((book) => book.title !== title);
+
+const removeBook = (id) => {
+  books = books.filter((book) => book.id !== id);
 };
+
 const showBooks = () => {
   for (const book of books) {
     console.log(`${book.title} by ${book.author}`);
   }
 };
+
 const displayBooks = () => {
   const booksList = document.querySelector('.books');
+  booksList.innerHTML = '';
   for (let book of books) {
     let bookElement = document.createElement('div');
     bookElement.classList.add('book');
+
     let h2 = document.createElement('h2');
     h2.classList.add('title');
     h2.textContent = book.title;
+
     let h3 = document.createElement('h3');
     h3.classList.add('author');
     h3.textContent = book.author;
+
     let removeBtn = document.createElement('button');
-    removeBtn.classList.add('remove');
+    removeBtn.classList.add('remove-' + book.id);
     removeBtn.textContent = 'Remove';
+    removeBtn.addEventListener('click', () => {
+      removeBook(book.id);
+      displayBooks();
+    });
+
     bookElement.appendChild(h2);
     bookElement.appendChild(h3);
     bookElement.appendChild(removeBtn);
+
     booksList.appendChild(bookElement);
+
     let hr = document.createElement('hr');
     booksList.appendChild(hr);
   }
+  saveData();
 };
 
 /* form functions */
@@ -38,48 +62,30 @@ const form = document.querySelector('form');
 const author = form.querySelector('#author');
 const title = form.querySelector('#title');
 
-const addNew = () => {
+const rmvListener = () => {
+  for (let i = 0; i < books.length; i++) {
+    let removeBtn = document.querySelector('.remove-' + i);
+    console.log(removeBtn);
+  }
+};
+
+const saveData = () => {
+  for (const book in books) {
+    localStorage.setItem(book.title, JSON.stringify(book.author));
+  }
+};
+
+window.addEventListener('load', () => {
+  const formData = JSON.parse(localStorage.getItem('Data')) || [];
+  books = formData;
+});
+
+window.onload = displayBooks();
+const addBtn = document.querySelector('#add-btn');
+addBtn.addEventListener('click', (e) => {
+  e.preventDefault();
   bookTitle = title.value;
   bookAuthor = author.value;
   addBook(bookTitle, bookAuthor);
   displayBooks();
-};
-
-const removeBtnList = document.querySelectorAll('.remove');
-for ( i = 0; i < removeBtnList.length; i++) {
-  removeBtnList[i].addEventListener('click', remove(i));
-}
-
-
-const remove = (i) => {
-  const book = books[i];
-  removeBook(book.title);
-  displayBooks();
-}
-
-const saveDate = () => {
-  const formData = {
-    formAuthor: author.value,
-    formTitle: title.value
-  };
-  localStorage.setItem('formData', JSON.stringify(formData));
-};
-
-author.addEventListener('change', saveDate);
-title.addEventListener('change', saveDate);
-
-window.addEventListener('load', () => {
-  const formData = JSON.parse(localStorage.getItem('formData'));
-  author.value = formData.name;
-  title.value = formData.email;
 });
-
-
-// addBook("The Lord of the Rings", "J.R.R. Tolkien");
-// addBook("The Hobbit", "J.R.R. Tolkien");
-// addBook("The Catcher in the Rye", "J.D. Salinger");
-// removeBook("The Hobbit");
-// showBooks();
-window.onload = displayBooks();
-const addBtn = document.querySelector('#add-btn');
-addBtn.onclick = addNew;
