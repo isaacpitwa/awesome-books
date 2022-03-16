09c8404861e119937b4d6d08b59e9563d86ae4ac
mrEckendonk <mike.e@vecks.com.br>
2022-03-16 13:52:36 -0300

    created navigation

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ class Methods {
     this.author = author;
     this.id = id;
   }
-
+ 
   addBook = (bookTitle, bookAuthor, bookId) => {
     const book = {
       title: bookTitle,
@@ -13,19 +13,19 @@ class Methods {
     };
     this.books.push(book);
   };
-
+ 
   removeBook = (id) => {
     this.books.splice(this.books[id - 1], 1);
   };
 }
-
+ 
 const methods = new Methods();
 methods.books = [];
-
+ 
 const saveData = () => {
   localStorage.setItem('myBooks', JSON.stringify(methods.books));
 };
-
+ 
 const displayBooks = () => {
   const booksList = document.querySelector('.books');
   booksList.innerHTML = '';
@@ -36,11 +36,11 @@ const displayBooks = () => {
     if ((i + 1) % 2 !== 0) {
       bookElement.classList.add('odd');
     }
-
+ 
     const h2 = document.createElement('h2');
     h2.classList.add('title');
     h2.textContent = `"${book.title}"  by ${book.author}`;
-
+ 
     const removeBtn = document.createElement('button');
     removeBtn.classList.add(`remove-${book.id}`);
     removeBtn.textContent = 'Remove';
@@ -48,15 +48,15 @@ const displayBooks = () => {
       methods.removeBook(book.id);
       displayBooks();
     });
-
+ 
     bookElement.appendChild(h2);
     bookElement.appendChild(removeBtn);
-
+ 
     booksList.appendChild(bookElement);
   }
   saveData();
 };
-
+ 
 const getData = () => {
   const formData = JSON.parse(localStorage.getItem('myBooks'));
   if (formData == null) {
@@ -65,18 +65,18 @@ const getData = () => {
     methods.books = formData;
   }
 };
-
+ 
 // window.onload = getData();
 window.onbeforeunload = () => {
   getData();
   displayBooks();
 };
-
+ 
 /* form functions */
 const form = document.querySelector('form');
 const author = form.querySelector('#author');
 const title = form.querySelector('#title');
-
+ 
 const addBtn = document.querySelector('#add-btn');
 addBtn.addEventListener('click', (e) => {
   e.preventDefault();
@@ -87,14 +87,14 @@ addBtn.addEventListener('click', (e) => {
   displayBooks();
   saveData();
 });
-
+ 
 getData();
 displayBooks();
-
+ 
 // Creating Date object for date
-
+ 
 // const date = new Date();
-
+ 
 // const datePresenter = ()=>{
 //   let month = date.getMonth.toString();
 //   let exactDate = date.getDate();
@@ -102,7 +102,7 @@ displayBooks();
 // }
 // document.body.insertBefore(date, h1);
 // setInterval(datePresenter, 1000);
-
+ 
 // Referrence to navbar
 const list = document.querySelector('.list');
 const add = document.querySelector('.add');
@@ -110,15 +110,30 @@ const contact = document.querySelector('.contact');
 const wholeForm = document.querySelector('#form-container');
 const books = document.querySelector('#books-container');
 const body = document.querySelector('body');
-
+const footer = document.querySelector('footer');
+const contactContainer = document.querySelector('.contact-section');
+ 
+ 
 const listActive = () => {
   body.removeChild(wholeForm);
-  body.appendChild(books);
+  body.removeChild(contactContainer);
+  body.insertBefore(books, footer);
+  contactContainer.classList.remove('contact-section-active');
 };
 const formActive = () => {
   body.removeChild(books);
-  body.appendChild(wholeForm);
+  body.removeChild(contactContainer);
+  body.insertBefore(wholeForm, footer);
+  contactContainer.classList.remove('contact-section-active');
 };
-
+ 
+const contactActive = () => {
+  body.removeChild(books);
+  body.removeChild(wholeForm);
+  body.insertBefore(contactContainer, footer);
+  contactContainer.classList.add('contact-section-active');
+};
+ 
 list.addEventListener('click', listActive);
 add.addEventListener('click', formActive);
+contact.addEventListener('click', contactActive);
